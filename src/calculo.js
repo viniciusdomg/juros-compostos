88b2e953714d242ec8ea4e9c4a9a923c0225b052
Vinicius de Oliveira <130383526+viniciusdomg@users.noreply.github.com>
2025-01-21 16:57:38 -0300

    feat: adiciona exportação do módulo para uso da função

--- src/calculo.js
+++ src/calculo.js
@@ -1,12 +1,6 @@
 function calcularJurosCompostos(capitalInicial, taxaJuros, periodos) {
-    
     const montante = capitalInicial * Math.pow(1 + taxaJuros, periodos);
     return montante.toFixed(2); 
 }
 
-const capitalInicial = 1000;
-const taxaJuros = 0.05; 
-const periodos = 12; 
-
-const montanteFinal = calcularJurosCompostos(capitalInicial, taxaJuros, periodos);
-console.log(`O montante final após ${periodos} períodos será de R$ ${montanteFinal}`);
+module.exports = calcularJurosCompostos;
